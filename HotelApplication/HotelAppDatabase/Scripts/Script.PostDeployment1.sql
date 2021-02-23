2cc6fb467288a71b9e807650bdb19856817d59c1
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-02-23 05:05:52 -0500

    updating files

--- HotelApplication/HotelAppDatabase/Scripts/Script.PostDeployment1.sql
+++ HotelApplication/HotelAppDatabase/Scripts/Script.PostDeployment1.sql
@@ -13,9 +13,10 @@ Post-Deployment Script Template
 if not exists (select 1 from dbo.RoomTypes)
 begin
     insert into dbo.RoomTypes (Title, Description, Price)
-    values  ('King Size Bed', 'A room with a king-size bed and a window.', 100),
-            ('Two Queen-size Bed', 'A room with two queen-size beds and double window.', 115),
-            ('Executive Suite', 'Two rooms; each with two king-size beds and a window.', 205);
+    values
+    ('King Size Bed', 'A room with a king-size bed and a window.', 100),
+    ('Two Queen-size Bed', 'A room with two queen-size beds and double window.', 115),
+    ('Executive Suite', 'Two rooms; each with two king-size beds and a window.', 205);
 end
 
 if not exists (select 1 from dbo.Rooms)
@@ -24,12 +25,13 @@ begin
     declare @roomId2 int;
     declare @roomId3 int;
 
-    select @roomId1 from dbo.RoomTypes where Title = 'King Size Bed';
-    select @roomId2 from dbo.RoomTypes where Title = 'Two Queen-size Beds';
-    select @roomId3 from dbo.RoomTypes where Title = 'Executive Suite';
+    select @roomId1 = Id from dbo.RoomTypes where Title = 'King Size Bed';
+    select @roomId2 = Id from dbo.RoomTypes where Title = 'Two Queen-size Bed';
+    select @roomId3 = Id from dbo.RoomTypes where Title = 'Executive Suite';
 
     insert into dbo.Rooms (RoomNumber, RoomTypeId)
-    values ('101', @roomId1),    
+    values
+    ('101', @roomId1),    
     ('102', @roomId1),
     ('103', @roomId1),
     ('201', @roomId2),
