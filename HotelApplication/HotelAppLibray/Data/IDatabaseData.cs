17639e4f13ddbed11396a902edfdccdc701636e1
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-02-24 06:37:10 -0500

    updating files for today

---
+++ HotelApplication/HotelAppLibray/Data/IDatabaseData.cs
@@ -0,0 +1,14 @@
+﻿using HotelAppLibray.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HotelAppLibray.Data
+{
+    public interface IDatabaseData
+    {
+        void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId);
+        void CheckinGuest(int bookingId);
+        List<RoomTypeModel> GetAvailableRooms(DateTime startDate, DateTime endDate);
+        List<BookingFullModel> SearchBookings(string lastName);
+    }
+}
