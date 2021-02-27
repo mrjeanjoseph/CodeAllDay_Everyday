e0bc3b7c468a99dec5a4ecc17900dc175fdf41f9
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-02-27 06:56:08 -0500

    fixed the 'ConnectionString must be set before Open can be called' issue

--- HotelApplication/HotelAppLibray/Data/SqliteData.cs
+++ HotelApplication/HotelAppLibray/Data/SqliteData.cs
@@ -1,4 +1,5 @@
-﻿using HotelAppLibray.Models;
+﻿using HotelAppLibray.Databases;
+using HotelAppLibray.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,14 @@ namespace HotelAppLibray.Data
 {
     public class SqliteData : IDatabaseData
     {
+        private const string connectionStringName = "SQLiteDb";
+        private readonly ISqliteDataAccess db;
+
+        public SqliteData(ISqliteDataAccess db)
+        {
+            this.db = db;
+        }
+
         public void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
         {
             throw new NotImplementedException();
@@ -19,7 +28,21 @@ namespace HotelAppLibray.Data
 
         public List<RoomTypeModel> GetAvailableRooms(DateTime startDate, DateTime endDate)
         {
-            throw new NotImplementedException();
+            string sqlite = @"select r.*
+		                    from dbo.Rooms r
+		                    inner join dbo.RoomTypes t on t.id = r.RoomTypeId
+		                    where r.RoomTypeId = @roomTypeId
+		                    and r.Id not in (
+		                    select b.RoomId
+		                    from dbo.Bookings b
+		                    where	(@startDate < b.StartDate and @endDate > b.EndDate)
+			                    or (b.StartDate <= @endDate and @endDate < b.EndDate)
+			                    or (b.StartDate <= @startDate and @startDate < b.EndDate)
+			                    );";
+
+            return db.LoadData<RoomTypeModel, dynamic>(sqlite,
+                                     new { startDate, endDate },
+                                     connectionStringName);
         }
 
         public RoomTypeModel GetRoomTypeById(int id)
