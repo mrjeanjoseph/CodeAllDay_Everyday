6ddbc410eee89fef44098f53d41cb29287eae6ad
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-03-07 05:45:39 -0500

    getting creative here

--- LoginForm/js/validate.js
+++ LoginForm/js/validate.js
@@ -1,14 +1,14 @@
 function autenticate()
 {
     var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
+    var enterpass=document.getElementById("enterpass").value;
+    var reenterpass=document.getElementById("reenterpass").value;
 
-    if(username=="user"&& password=="password"){
+    if(username=="user"&& enterpass=="password"&& reenterpass=="password"){
         alert("login successful!");
         return false;
     }
     else{
         alert("login failed!");
-        alert("Try again");
     }
 }
