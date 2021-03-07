3a7adb1aa6749784cdfd9693b9c3032ec290be21
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-03-07 11:02:57 -0500

    There's an issue with js not invoking the popup method

--- LoginForm/js/form.js
+++ LoginForm/js/form.js
@@ -3,34 +3,13 @@ function autenticate() {
     var username = document.getElementById("username").value;
     var enterpass = document.getElementById("enterpass").value;
     var reenterpass = document.getElementById("reenterpass").value;
-    var failed = "https://www.google.com/";
-    var good = "https://www.youtube.com/";
 
     if (username == "user"&& enterpass == "pass"&& reenterpass == "pass") {
-            location.href = 'https://www.google.com/';
+        
+        dropdown.style.display = "block";
+          
     }
     else {
-        location.href = 'https://www.youtube.com/';
+        alert("login failed!");
     }
 }
-    //=============================================
-    // popup validation box
-    var dropdown = document.getElementById("myDropdown");
-    var btn = document.getElementById("myBtn");
-    var span =
-        document.getElementsByClassName("close")[0];
-
-    btn.onclick = function () {
-        dropdown.style.display = "block";
-    }
-
-    span.onclick = function () {
-        dropdown.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == dropdown) {
-            dropdown.style.display = "none";
-        }
-    }
-
