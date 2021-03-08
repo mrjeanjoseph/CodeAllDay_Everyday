66c082295cc874a7dba3b8e5b11fe2909ec7dfaf
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-03-08 06:34:13 -0500

    Still working on the .masterpage

--- OnlineBookstore/Bookstore.Web/Site1.Master.designer.cs
+++ OnlineBookstore/Bookstore.Web/Site1.Master.designer.cs
@@ -31,5 +31,14 @@ namespace Bookstore.Web
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
